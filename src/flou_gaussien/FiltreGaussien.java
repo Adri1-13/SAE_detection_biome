342f16c2aee443067374d494a90e73b166cf777c
Louis Ardhuin <louis.ardhuin4@etu.univ-lorraine.fr>
2026-06-12 09:43:16 +0200

    Début flou gaussien

--- src/flou_gaussien/FiltreGaussien.java
+++ src/flou_gaussien/FiltreGaussien.java
@@ -5,11 +5,21 @@ public class FiltreGaussien {
     double[][] filtre;
 
     public FiltreGaussien(int taille) {
-        filtre = new double[taille][taille];   // allocation manquante
+        filtre = new double[taille][taille];
         int centre = taille / 2;
+        double somme = 0.0;
+
         for (int i = 0; i < taille; i++) {
             for (int j = 0; j < taille; j++) {
-                filtre[i][j] = calcul_gauss(i - centre, j - centre, 3.0);  // centrage
+                filtre[i][j] = calcul_gauss(i - centre, j - centre, 3.0);
+                somme += filtre[i][j];
+            }
+        }
+
+        // normalisation : la somme des coefficients vaut 1
+        for (int i = 0; i < taille; i++) {
+            for (int j = 0; j < taille; j++) {
+                filtre[i][j] /= somme;
             }
         }
     }
